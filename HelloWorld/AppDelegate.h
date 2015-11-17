b14cb8d830d69f8195f1fbe76470ecc390b9331b
Your Name <you@example.com>
2015-11-17 14:16:00 +0800

    Initial Commit

---
+++ HelloWorld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloWorld
+//
+//  Created by Arvin on 15/11/17.
+//  Copyright (c) 2015年 Arvin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
